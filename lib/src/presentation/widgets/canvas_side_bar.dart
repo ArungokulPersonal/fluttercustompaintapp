03b23684ce7b4cd7554a4eae2231d847393b0efe
ArungokulPersonal <goksversatile@gmail.com>
2024-09-11 13:07:23 +0530

    Removed unused imports and optimized the code.

--- lib/src/presentation/widgets/canvas_side_bar.dart
+++ lib/src/presentation/widgets/canvas_side_bar.dart
@@ -8,7 +8,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart' hide Image;
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-import 'package:custompaintapp/main.dart';
 import 'package:custompaintapp/src/src.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:image_picker/image_picker.dart';
